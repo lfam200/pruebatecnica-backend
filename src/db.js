7ea6e841ab4799fa86e333a55f1ed996d62f2a32
Luis Abanto <lfam200@hotmail.com>
2022-03-03 23:01:13 -0500

    Fix Deploy

--- src/db.js
+++ src/db.js
@@ -2,15 +2,36 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  NODE_ENV,
+  DATABASE_URL,
+} = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  {
-    logging: false,
-    native: false,
-  }
-);
+let sequelize =
+  NODE_ENV === "local"
+    ? new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+        {
+          logging: false,
+          native: false,
+        }
+      )
+    : new Sequelize(`${DATABASE_URL}`, {
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+        logging: false,
+        native: false,
+      });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
